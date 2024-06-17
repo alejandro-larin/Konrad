06f9d49683c11bf427035c362a44594dcb1094d1
4lx7 <4lx7.zzz@gmail.com>
2024-06-17 13:16:52 +0200

    add Style Color to the letters

--- src/main.py
+++ src/main.py
@@ -1,39 +1,41 @@
 import os
 from utils.travel import TrabelPdf
+from colorama import init, Fore, Style
 os.system('cls')
 #Class
 class Menu:
     def Menu_1(self):
         print(
-'''
-Welcome to Konrad!
--------------------------------------------------------------------------
-1. Link Base.
+f'''
+{Style.BRIGHT}{Fore.CYAN}Welcome to Konrad!
+-------------------------------------------------------------------------{Fore.RESET}
+{Fore.YELLOW}1. Link Base.
 2. Abbreviation.
-3. PDF name conversion option.
+3. PDF name conversion option.{Fore.RESET}
+{Fore.RED}4. Exit{Fore.RESET}
 ''')
     def Menu_2(self):
         print(
-'''
-Konrand Sub Menu
--------------------------------------------------------
-1. Curriculum
-2. Planning Studing
-3. Returning 
+f'''
+{Fore.CYAN}Konrand Sub Menu
+-------------------------------------------------------{Fore.RESET}
+{Fore.YELLOW}1. Curriculum
+2. Planning Studing{Fore.RESET}
+{Fore.RED}3. Returning {Fore.RESET}
 ''')
 menu = Menu()
 
 # Especificar el directorio
 DIR = './pdf'
-# TrabelPdf(DIR)
+ 
 
 
 
 menu.Menu_1()
-menuResponse= int(input("Plase write one option of the menu: "))
-response=True
+menuResponse= int(input(f"{Fore.MAGENTA}Plase write one option of the menu: {Fore.RESET}"))
 
-while response == True:
+
+while True:
     #Menu Structure 
     if menuResponse == 1:
         print('Option 1')
@@ -45,28 +47,30 @@ while response == True:
         os.system('cls')
         #Print menu
         menu.Menu_2()
-        menuResponse_2= int(input("Plase write one option of the menu: "))
+        subMenuResponse= int(input("Plase write one option of the menu: "))
         
-        if menuResponse_2 == 1:
+        if subMenuResponse == 1:
             print('b-1')
             continue
-        elif menuResponse_2 == 2:
-            print ('b-2')
+        elif subMenuResponse == 2:
+            print('Change...')
+            TrabelPdf(DIR)
             continue
-        elif menuResponse_2 == 3:
-            print('o')
+        elif subMenuResponse == 3:
             os.system('cls')
-
         else:
             os.system('cls')
             continue
+    elif menuResponse == 4:
+        break
     else:
         os.system('cls')
-    #Response of the user
     #Menu Option
     menu.Menu_1()
-    menuResponse= int(input("Plase write one option of the menu: "))
-print('Exit...')
+    menuResponse= int(input(f"{Fore.MAGENTA}Plase write one option of the menu: {Fore.RESET}"))
+print(f'Exit... {Style.RESET_ALL}')
+os.system('cls')
+
 
 
 
